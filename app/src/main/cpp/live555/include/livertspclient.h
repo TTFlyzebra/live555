8921452e30f41f5b76c484e09236a44027642905
TTFlyZebra <852184385@qq.com>
2018-05-13 23:51:25 +0800

    rtspclient

---
+++ app/src/main/cpp/live555/include/livertspclient.h
@@ -0,0 +1,15 @@
+#ifndef _STREAM_PARSER_HH
+
+#include "livertspclient.h"
+
+#define _STREAM_PARSER_HH
+
+#include "RTSPClient.hh"
+#include "BasicUsageEnvironment.hh"
+#include "liveMedia.hh"
+
+int connect(const char *url);
+
+#endif
+
+
